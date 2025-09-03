df980c86dd2b1a83dbe1ff633764139709e8d00d
Vinícius Datori <vinidatori@gmail.com>
2025-09-03 12:39:36 -0300

    Definida a função do botão 'Reiniciar'.

--- Sorteador de Números/app.js
+++ Sorteador de Números/app.js
@@ -1,15 +1,22 @@
+// Definição da lista de números sorteados e variáveis:
+
 let numSorteados = [];
+let qtdDeNumero;
+let intMin;
+let intMax;
+let botaoSortear;
+let botaoReiniciar;
 
 // Função que sorteia os numeros:
 
 function sortear() {
 
     // Declaração de variáveis cujos valores serão os números fornecidos pelo usuário:
-    let qtdDeNumero = document.querySelector("#input1").value;
-    let intMin = document.querySelector("#input2").value;
-    let intMax = document.querySelector("#input3").value;
+    qtdDeNumero = document.querySelector("#input1").value;
+    intMin = document.querySelector("#input2").value;
+    intMax = document.querySelector("#input3").value;
     
-    // Estrutura de repetição que sorteia a quantidade de números definida pelo usuário: 
+    // Estrutura de repetição que sorteia a quantidade de números definida pelo usuário (FALTA IMPEDIR QUE SEJA SORTEADO O MESMO NÚMERO):
     while(qtdDeNumero != 0) {
         intMin = Math.ceil(intMin);
         intMax = Math.floor(intMax) + 1;
@@ -22,16 +29,14 @@ function sortear() {
     resultadoSorteio("#resultado","Números sorteados: " + numSorteados);
     
     // Manipulação do Botão Reiniciar:
-    let botaoReiniciar = document.getElementById("btn-reiniciar");
+    botaoReiniciar = document.getElementById("btn-reiniciar");
     botaoReiniciar.style.cursor = "pointer";
     botaoReiniciar = document.getElementById("btn-reiniciar").removeAttribute("disabled");
 
     // Manipulação do Botão Sortear:
-    let botaoSortear = document.getElementById("btn-sortear");
+    botaoSortear = document.getElementById("btn-sortear");
     botaoSortear.style.cursor = "not-allowed";
     botaoSortear = document.getElementById("btn-sortear").setAttribute("disabled", true);
-
-
 }
 
 // Função que exibe os números sorteados na tela:
@@ -43,3 +48,22 @@ function resultadoSorteio(tag, texto) {
 
 // Função para reiniciar o sorteador:
 
+function reiniciar() {
+    console.log("Está funcionando.");
+    // Limpando a lista e os campos:
+    numSorteados = [];
+    qtdDeNumero.value = "";
+    intMin.value = "";
+    intMax.value = "";
+
+    // Manipulando a mensagem ao usuário para retornar ao padrão:
+    resultadoSorteio("#resultado","Números sorteados:  nenhum até agora");
+
+    // Manipulando os botões:
+    botaoReiniciar = document.getElementById("btn-reiniciar")
+    botaoReiniciar.style.cursor = "not-allowed";
+    botaoReiniciar = document.getElementById("btn-reiniciar").setAttribute("disabled", true);
+    botaoSortear = document.getElementById("btn-sortear")
+    botaoSortear.style.cursor = "pointer";
+    botaoSortear = document.getElementById("btn-sortear").removeAttribute("disabled");
+}
