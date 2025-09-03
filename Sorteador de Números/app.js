ea5fe345827f6604ecb911b41546e623f50a356e
Vinícius Datori <vinidatori@gmail.com>
2025-09-03 15:09:07 -0300

    Corrigido problemas na quantidade de números e intervalos definidos pelo usuário.

--- Sorteador de Números/app.js
+++ Sorteador de Números/app.js
@@ -8,36 +8,51 @@ let botaoReiniciar;
 
 function sortear() {
 
-    // Declaração de variáveis cujos valores serão os números fornecidos pelo usuário:
+    // Declaração de variáveis cujos valores serão fornecidos pelo usuário:
     let qtdDeNumero = document.querySelector("#input1").value;
     let intMin = document.querySelector("#input2").value;
     let intMax = document.querySelector("#input3").value;
     
-    // Estrutura de repetição que sorteia a quantidade de números definida pelo usuário (FALTA IMPEDIR QUE SEJA SORTEADO O MESMO NÚMERO):
-    while(qtdDeNumero != 0) {
-        intMin = Math.ceil(intMin);
-        intMax = Math.floor(intMax) + 1;
-        let numeroSorteado = Math.floor(Math.random() * (intMax - intMin) + intMin); // É necessário manter o "+ min" para que o valor sorteado seja maior ou igual ao número mínimo definido.
-        numSorteados.push(numeroSorteado);
-        qtdDeNumero--;
+    // Para evitar bugs:
+    if(qtdDeNumero > intMax) {
+        alert("A quantidade de números sorteados não pode ser maior que o intervalo máximo definido.");
+    } else {
+        // Estrutura de repetição que sorteia a quantidade de números definida pelo usuário sem repetir o mesmo número na mesma rodada:
+        while (qtdDeNumero != 0) {
+
+            intMin = Math.ceil(intMin);
+            intMax = Math.floor(intMax);
+            let numeroSorteado = Math.floor(Math.random() * (intMax - intMin + 1) + intMin);
+
+            if (numSorteados.includes(numeroSorteado)) {
+
+                numeroSorteado = "";
+                intMin = Math.ceil(intMin);
+                intMax = Math.floor(intMax);
+                numeroSorteado = Math.floor(Math.random() * (intMax - intMin + 1) + intMin);
+
+            } else {
+                numSorteados.push(numeroSorteado);
+                qtdDeNumero--;
+            }
+        }
+
+        // Manipulação de texto que mostra ao usuário os números sorteados:
+        resultadoSorteio("#resultado", "Números sorteados: " + numSorteados);
+
+        // Manipulação do Botão Reiniciar:
+        botaoReiniciar = document.getElementById("btn-reiniciar");
+        botaoReiniciar.style.cursor = "pointer";
+        botaoReiniciar = document.getElementById("btn-reiniciar").removeAttribute("disabled");
+
+        // Manipulação do Botão Sortear:
+        botaoSortear = document.getElementById("btn-sortear");
+        botaoSortear.style.cursor = "not-allowed";
+        botaoSortear = document.getElementById("btn-sortear").setAttribute("disabled", true);
     }
-    
-    // Manipulação de texto que mostra ao usuário os números sorteados:
-    resultadoSorteio("#resultado","Números sorteados: " + numSorteados);
-    
-    // Manipulação do Botão Reiniciar:
-    botaoReiniciar = document.getElementById("btn-reiniciar");
-    botaoReiniciar.style.cursor = "pointer";
-    botaoReiniciar = document.getElementById("btn-reiniciar").removeAttribute("disabled");
-
-    // Manipulação do Botão Sortear:
-    botaoSortear = document.getElementById("btn-sortear");
-    botaoSortear.style.cursor = "not-allowed";
-    botaoSortear = document.getElementById("btn-sortear").setAttribute("disabled", true);
-
 }
 
-// Função que exibe os números sorteados na tela:
+// Função para exibir os números sorteados na tela:
 
 function resultadoSorteio(tag, texto) {
     let mensagem = document.querySelector(tag);
@@ -63,7 +78,7 @@ function reiniciar() {
     botaoSortear = document.getElementById("btn-sortear").removeAttribute("disabled");
 }
 
-// Função que esvazia os campos de input:
+// Função que esvazia os campos de input ao reiniciar:
 
 function limparCampos() {
     document.querySelector("#input1").value = "";
