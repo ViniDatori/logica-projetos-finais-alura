fd7b850a9ff3add4131a85a10889959c4fdb10f4
Vinícius Datori <vinidatori@gmail.com>
2025-09-03 11:16:27 -0300

    Usuário pode determinar a quantidade de números sorteados.

--- Sorteador de Números/app.js
+++ Sorteador de Números/app.js
@@ -4,15 +4,23 @@ let numSorteados = [];
 
 function sortear() {
 
-    // let qtdDeNumero = document.getElementById("quantidade").value;
-    
+    let qtdDeNumero = document.querySelector("#input1").value;
     let intMin = document.querySelector("#input2").value;
     let intMax = document.querySelector("#input3").value;
     
-    intMin = Math.ceil(intMin);
-    intMax = Math.floor(intMax) + 1;
-    let numeroSorteado = Math.floor(Math.random() * (intMax - intMin) + intMin); // É necessário manter o "+ min" para que o valor sorteado seja maior ou igual ao número mínimo definido.
+    while(qtdDeNumero != 0) {
+        intMin = Math.ceil(intMin);
+        intMax = Math.floor(intMax) + 1;
+        let numeroSorteado = Math.floor(Math.random() * (intMax - intMin) + intMin); // É necessário manter o "+ min" para que o valor sorteado seja maior ou igual ao número mínimo definido.
+        numSorteados.push(numeroSorteado);
+        qtdDeNumero--;
+    }
     
-    numSorteados.push(numeroSorteado);
-    console.log(numeroSorteado);
+    resultadoSorteio("#resultado","Números sorteados: " + numSorteados);
+
+}
+
+function resultadoSorteio(tag, texto) {
+    let mensagem = document.querySelector(tag);
+    mensagem.innerHTML = texto;
 }
